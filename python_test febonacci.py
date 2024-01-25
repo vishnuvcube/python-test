a9321b61435560422cb9f038eed2cde35b3a3810
vishnuvcube <143273170+vishnuvcube@users.noreply.github.com>
2024-01-25 12:31:32 +0530

    Update python_test febonacci.py

--- python_test febonacci.py
+++ python_test febonacci.py
@@ -1,17 +1,13 @@
-try:
-    n = int(input('Enter the number of terms in the Fibonacci sequence: '))
-
-    if n <= 0:
-        print('Please enter a positive integer.')
-
-    else:
-        fibonacci_sequence = [0, 1]
-
-        for _ in range(2, n):
-            next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-            fibonacci_sequence.append(next_term)
-
-        print(f'Fibonacci sequence up to {n} terms: {fibonacci_sequence}')
-
-except ValueError:
-    print('Invalid input. Please enter a valid integer.')
+num=int(input("enter the number: "))
+x=0
+y=1
+z=0
+print(x,y, end=",")
+if num<=0:
+    print("enter positive integer")
+else:
+    for i in range(2, num):
+        z=x+y
+        print(z, end="," )
+        x=y 
+        y=z 
